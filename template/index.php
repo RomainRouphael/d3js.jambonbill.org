fb780f9513bcb0d82b95db1644c68583575b30ba
jambonbill <jambonbill@gmail.com>
2015-03-20 19:18:57 +0800

    plein de bordel

---
+++ template/index.php
@@ -0,0 +1,65 @@
+<?php
+//d3js.jambonbill.org template
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>d3js experimentations</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<style>
+body { padding-top: 70px; }  
+</style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+   <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">d3js.jambonbill.org</a>
+    </div>
+  </div><!-- /.container-fluid -->
+</nav>
+
+<div class='container'>
+
+<h1>Template title</h1>
+<hr />
+<div id='chart'></div>
+
+
+<div id='info'>
+<!-- Markdown syntax -->
+<pre>
+http://www.codingdrama.com/bootstrap-markdown/
+http://kevo.io/pagedown-bootstrap/
+
+Databits is an invitation-based website where creative coders, engineers, data journalists, and data scientists multiply impact by sharing work and connecting.
+</pre>
+</div>
+
+<div id='code'>
+<!-- Show js code here -->
+<pre>
+http://www.codingdrama.com/bootstrap-markdown/
+
+01 Code
+02 Code
+03 Code
+04 Code
+05 ...
+</pre>
+</div>
+
+
+
+<!--footer-->
+<script type="text/javascript" src="../js/jquery.min.js"></script>
+<!-- d3 -->
+<script src="../js/d3.min.js"></script>
+<script src='template.js'></script>
